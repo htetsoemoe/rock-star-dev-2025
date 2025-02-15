bfe34c4d9e5af6daa6824502d639441dcf6f4361
htetsoemoe <soemoehtet.mdy@gmail.com>
2025-02-15 17:40:05 +0630

    chapter06 setup and run

---
+++ chapter06-MUI/src/components/List.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const List = ({ children }) => {
+    return (
+        <ul
+            style={{
+                listStyle: 'none',
+                padding: 0,
+                margin: 0,
+                border: '1px solid #ddd',
+                borderRadius: 8,
+                overflow: 'hidden',
+            }}
+        >
+            {children}
+        </ul>
+    )
+}
+
+export default List
